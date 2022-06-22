6c6cbe6c6fe1ef6e4370e6f53cc79e29f284e2ca
ecoinsight <107825408+ecoinsight@users.noreply.github.com>
2022-06-22 07:48:42 +0530

    files updated

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7r9h+*7al5d+o7fnn4%1$#t-%(+^^4r-+-b1g8*j2q8bzan-hx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://searchengpo.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
